3078a1773664ee5bd697e2672550ed21257f44f6
Your Name <youremail@yourdomain.com>
2023-06-26 18:58:39 +0530

    Init

---
+++ trove12-main/src/components/Team_mem.js
@@ -0,0 +1,24 @@
+const Team_mem = [
+    {
+        id:1,
+        Name:"Jiya Sharma",
+    },
+    {
+        id:2,
+        Name:"Sanchit Banga",
+    },
+    {
+        id:3,
+        Name:"Sanjay Kumar",
+    },
+    {
+        id:4,
+        Name:"Aishwarya Jha",
+    },
+    {
+        id:5,
+        Name:"Amit Giri",
+    }
+    
+]
+export default Team_mem
